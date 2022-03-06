b1107c9bdb692b9fff23b1390ae38e3df04ee97d
HM CHOI <vivalahm@gmail.com>
2022-03-06 14:35:54 +0900

    Add input Scanner

--- src/main/java/RamenProgram.java
+++ src/main/java/RamenProgram.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class RamenProgram {
+
     public static void main(String[] args) {
+        int num;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("라면 몇 개 끓일까요?");
+        num = input.nextInt();
+
+        System.out.println(num + "개 주문 완료! 조리시작!");
         try {
-            RamenCook ramenCook = new RamenCook(50);
+            RamenCook ramenCook = new RamenCook(num);
             new Thread(ramenCook, "A").start();
             new Thread(ramenCook, "B").start();
             new Thread(ramenCook, "C").start();
